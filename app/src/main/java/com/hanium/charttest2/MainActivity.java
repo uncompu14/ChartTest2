782465c6a2283db4c08a3444b594557a898135a3
check <uncompu14@naver.com>
2021-10-15 00:36:18 +0900

    Initial commit

---
+++ app/src/main/java/com/hanium/charttest2/MainActivity.java
@@ -0,0 +1,109 @@
+package com.hanium.charttest2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.security.cert.PolicyNode;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText yValue;
+    Button btn_insert;
+
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
+    SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
+    GraphView graphView;
+    LineGraphSeries series;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        yValue = (EditText) findViewById(R.id.y_value);
+        btn_insert = (Button) findViewById(R.id.btn_insert);
+        graphView = (GraphView) findViewById(R.id.graphView);
+
+        series = new LineGraphSeries();
+        graphView.addSeries(series);
+
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("chartTable");
+
+        setListeners();
+
+        graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
+            @Override
+            public String formatLabel(double value, boolean isValueX) {
+
+                if (isValueX) {
+                    return sdf.format(new Date((long) value));
+                }
+                return super.formatLabel(value, isValueX);
+            }
+        });
+
+    }
+
+    private void setListeners() {
+        btn_insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String id = reference.push().getKey();
+                long x = new Date().getTime();
+                int y = Integer.parseInt(yValue.getText().toString());
+
+                PointValue pointValue = new PointValue(x, y);
+
+                reference.child(id).setValue(pointValue);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @org.jetbrains.annotations.NotNull DataSnapshot dataSnapshot) {
+                DataPoint[] dp = new DataPoint[(int) dataSnapshot.getChildrenCount()];
+                int index = 0;
+
+                for (DataSnapshot myDataSnapshot : dataSnapshot.getChildren()) {
+                    PointValue pointValue = myDataSnapshot.getValue(PointValue.class);
+
+                    dp[index] = new DataPoint(pointValue.getxValue(), pointValue.getyValue());
+                    index++;
+                }
+
+                series.resetData(dp);
+            }
+
+            @Override
+            public void onCancelled(@NonNull @org.jetbrains.annotations.NotNull DatabaseError error) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/hanium/charttest2/PointValue.java
@@ -0,0 +1,25 @@
+package com.hanium.charttest2;
+
+public class PointValue {
+
+    long xValue;
+    int yValue;
+
+    public PointValue() {
+
+    }
+
+    public PointValue(long xValue, int yValue) {
+        this.xValue = xValue;
+        this.yValue = yValue;
+    }
+
+    public long getxValue() {
+        return xValue;
+    }
+
+    public int getyValue() {
+        return yValue;
+    }
+}
+
